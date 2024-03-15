5f312761ed073f35cfbe941702641139ab177920
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-15 14:14:18 +0000

    auto-format code

--- projects-server/src/test/java/uk/ac/standrews/cs5031/group4/projectsserver/ProjectsServerApplicationTests.java
+++ projects-server/src/test/java/uk/ac/standrews/cs5031/group4/projectsserver/ProjectsServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjectsServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

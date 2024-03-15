73515c238cd24d9078912aef06e86a6050038188
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-15 13:46:34 +0000

    Init spring boot project

---
+++ projects-server/src/main/java/uk/ac/standrews/cs5031/group4/projectsserver/ProjectsServerApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.standrews.cs5031.group4.projectsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsServerApplication.class, args);
+	}
+
+}

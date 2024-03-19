322716cae8717b71787b4c6ad551b4e0bf895620
HD <854075704@qq.com>
2024-03-19 23:03:44 +0000

    List all projects. Register interest for student

--- projects-server/src/main/java/uk/ac/standrews/cs5031/group4/projectsserver/controller/ProjectController.java
+++ projects-server/src/main/java/uk/ac/standrews/cs5031/group4/projectsserver/controller/ProjectController.java
@@ -1,4 +1,51 @@
 package uk.ac.standrews.cs5031.group4.projectsserver.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.standrews.cs5031.group4.projectsserver.entities.Project;
+import uk.ac.standrews.cs5031.group4.projectsserver.service.ProjectService;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("/register-interest")
+    public ResponseEntity<?> registerInterest(@RequestBody Map<String, Object> payload, Principal principal) {
+        // Get the username of the currently logged in user from the SecurityContext.
+//        String username = principal.getName();
+        String username = "mm20";
+        // Get project_id from request body
+        int projectId = (Integer) payload.get("project_id");
+        try {
+            // Calling Service layer methods to handle student interest in the project
+            projectService.registerStudentInterest(username, projectId);
+            // If there are no exceptions, return 200 OK
+            return ResponseEntity.ok().build();
+        } catch (IllegalStateException e) {
+            // Returns 403 Forbidden if the item has already been assigned.
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (Exception e) {
+            // For other exceptions, return 500 Internal Server Error
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/proposed-projects")
+    public ResponseEntity<List<Project>> getProposedProjects(Principal principal) {
+        //String username = principal.getName();
+        String username = "jbloggs";
+        try {
+            List<Project> projects = projectService.getProjectsProposedByStaff(username);
+            return ResponseEntity.ok(projects);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
 }
---
+++ projects-server/src/main/java/uk/ac/standrews/cs5031/group4/projectsserver/entities/InterestedIn.java
@@ -0,0 +1,45 @@
+package uk.ac.standrews.cs5031.group4.projectsserver.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name = "interested_in")
+@IdClass(InterestedInId.class)
+public class InterestedIn {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "student_username", referencedColumnName = "username")
+    private User student;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    private Project project;
+
+    public InterestedIn() {
+    }
+
+    // Getters and setters
+    public User getStudent() {
+        return student;
+    }
+
+    public void setStudent(User student) {
+        this.student = student;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}

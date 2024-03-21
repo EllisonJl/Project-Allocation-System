60d561f807559d4b10ce5ce2ce00839579f3019e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-21 16:55:35 +0000

    Login tests

--- projects-server/src/test/java/uk/ac/standrews/cs5031/group4/projectsserver/controller/LoginControllerTest.java
+++ projects-server/src/test/java/uk/ac/standrews/cs5031/group4/projectsserver/controller/LoginControllerTest.java
@@ -1,70 +1,53 @@
 package uk.ac.standrews.cs5031.group4.projectsserver.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import uk.ac.standrews.cs5031.group4.projectsserver.repository.ProjectRepository;
 import uk.ac.standrews.cs5031.group4.projectsserver.repository.UserRepository;
 import uk.ac.standrews.cs5031.group4.projectsserver.service.JwtService;
+import uk.ac.standrews.cs5031.group4.projectsserver.service.ProjectService;
 import uk.ac.standrews.cs5031.group4.projectsserver.service.UserDetailsServiceImpl;
 
-@WebMvcTest(LoginController.class)
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration
-@WebAppConfiguration
+@WebMvcTest({ LoginController.class, ProjectController.class })
 public class LoginControllerTest {
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext context;
+    private MockMvc mockMvc;
 
     @MockBean
     AuthenticationManager authManager;
-
     @MockBean
     JwtService jwtService;
-
     @MockBean
     UserRepository userRepository;
-
     @MockBean
     ProjectRepository projectRepository;
-
     @MockBean
     UserDetailsServiceImpl userDetailsService;
+    @MockBean
+    ProjectService projectService;
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
-
+    /**
+     * Tests that users cannot access API routes when they aren't logged in.
+     */
     @Test
     public void requestProtectedRouteWithoutUser() throws Exception {
-        mockMvc.perform(get("/secret")).andExpect(status().isUnauthorized());
+        mockMvc.perform(get("/proposed-projects")).andExpect(status().isUnauthorized());
     }
 
+    /**
+     * Tests that users can access API routes when they are logged in.
+     */
     @Test
     @WithMockUser
     public void requestProtectedRouteWithUser() throws Exception {
-        mockMvc.perform(get("/secret")).andExpect(status().isOk());
+        mockMvc.perform(get("/proposed-projects")).andExpect(status().isOk());
     }
 }

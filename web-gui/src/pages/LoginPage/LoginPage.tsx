64030dbfe0ccef39b64497a13cc2be53ebdc0315
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-23 11:11:32 +0000

    implement log in

--- web-gui/src/pages/LoginPage/LoginPage.tsx
+++ web-gui/src/pages/LoginPage/LoginPage.tsx
@@ -1,24 +1,61 @@
+import { LoginForm } from "@components/LoginForm";
+import { useApiRequest } from "@hooks/use-api-request";
+import { useAuthToken } from "@hooks/use-auth-token";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const Label = styled.label`
-    display: block;
-`
-
 export function LoginPage() {
+    const api = useApiRequest();
+    const [_token, setToken] = useAuthToken();
+    const navigate = useNavigate();
+
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    async function login(username: string, password: string) {
+        const response = await api("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        });
+
+        if (response.status === 401) {
+            // Unauthorized
+            setErrorMessage("Invalid login details. Please try again.");
+            return;
+        } else if (!response.ok) {
+            // Catch any other error
+            setErrorMessage("Unknown error. Please try again.");
+            return;
+        }
+
+        // clear error if success
+        setErrorMessage(null);
+
+        // store the auth token we got
+        const body = await response.json();
+        setToken(body.token);
+
+        navigate("/");
+    }
+
     return (
         <>
             <h1>Log in</h1>
 
-            <form>
-                <Label>
-                    Username:
-                    <input type="text" />
-                </Label>
-                <Label>
-                    Password:
-                    <input type="password" />
-                </Label>
-            </form>
+            <LoginForm onSubmit={login} />
+
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
         </>
     );
 }
+
+const ErrorMessage = styled.div`
+    color: red;
+    font-weight: bold;
+`;
